20a9644ce8cd9f8639b9e1a67c65d51257580890
KaiGoni <epsilant101@gmail.com>
2025-02-27 23:44:05 -0500

    Updated code to new robot version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+// TO DO: Controllers testing, Autonomous
+
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       Anthony Wong                                              */
+/*    Author:       Anthony Wong & Zara Alom                                  */
 /*    Created:      12/30/2024                                                */
-/*    Description:  V5 project                                                */
+/*    Description:  VEX High Stakes Competition Code                          */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
@@ -17,23 +19,21 @@ brain Brain;
 
 // define your global instances of motors and other devices here
 
-motor leftMotorA = motor(PORT1, ratio6_1, false);
-motor leftMotorB = motor(PORT2, ratio6_1, false);
+motor leftMotorA = motor(PORT18, ratio6_1, true);
+motor leftMotorB = motor(PORT17, ratio6_1, true);
 motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
-motor rightMotorA = motor(PORT3, ratio6_1, true);
-motor rightMotorB = motor(PORT4, ratio6_1, true);
+motor rightMotorA = motor(PORT10, ratio6_1, false);
+motor rightMotorB = motor(PORT9, ratio6_1, false);
 motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
 drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 299.24, 31, 200, mm, 0.6);
 
-motor leftIntakeMotorA = motor(PORT17, ratio6_1, false);
-motor leftIntakeMotorB = motor(PORT18, ratio6_1, true);
-motor_group leftIntake = motor_group(leftIntakeMotorA, leftIntakeMotorB);
-motor rightIntakeMotorA = motor(PORT19, ratio6_1, false);
-motor rightIntakeMotorB = motor(PORT20, ratio6_1, false);
-motor_group rightIntake = motor_group(rightIntakeMotorA, rightIntakeMotorB);
+motor intake = motor(PORT20, ratio6_1, true);
+motor hook = motor(PORT16, ratio6_1, false);
 
 digital_out pnematicPort = digital_out(Brain.ThreeWirePort.A);
 controller Controller1 = controller(primary);
+
+float driveTrainSpeed = 100;
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -46,104 +46,10 @@ controller Controller1 = controller(primary);
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
-}
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-double fabs(double x) {
-    return (x < 0) ? -x : x;
-}
-
-double sqrt(double x) {
-    double guess = x / 2.0;
-    double epsilon = 0.0001;  // This controls the precision of the result
-    
-    while (fabs(guess * guess - x) > epsilon) {
-        guess = (guess + x / guess) / 2.0;
-    }
-    
-    return guess;
-}
-
-double atan(double x) {
-    double result = x;
-    double term = x;  // First term in the series
-    double x_squared = x * x;
-    int n = 1;  // Start with the first term
-
-    // Iterate to calculate a better approximation
-    while (term > 0.00001 || term < -0.00001) {
-        n += 2;
-        term = (-1) * term * x_squared * (n - 1) / n;
-        result += term;
-    }
-
-    return result;
-}
-
-double atan2(double y, double x) {
-    if (x > 0) {
-        return atan(y / x);  // First quadrant
-    } else if (x < 0 && y >= 0) {
-        return atan(y / x) + M_PI;  // Second quadrant
-    } else if (x < 0 && y < 0) {
-        return atan(y / x) - M_PI;  // Third quadrant
-    } else if (x == 0 && y != 0) {
-        return (y > 0) ? M_PI / 2 :  -M_PI / 2;  // Handle vertical line case
-    } else {
-        return 0;  // Undefined or zero case
-    }
-}
-
-void driveTo(int posX, int posY) {
-  // Get current values
-  int currentPosX = 0; // Current Pos to be determined
-  int currentPosY = 0;
-  int currentHeading = 0; // Current heading to be determined
-  double deltaX = posX - currentPosX;
-  double deltaY = posY - currentPosY;
   
-  // Calculate distance and angle
-  double distance = sqrt(deltaX * deltaX + deltaY * deltaY);
-  double angle = atan2(deltaY, deltaX) * 180 / M_PI;
-  
-  // Drivetrain.turnToHeading(angle, degrees);
-  // Drivetrain.driveFor(forward, distance, mm);
-}
-
-void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
-  Drivetrain.driveFor(forward, 300, mm);
-  wait(1000, msec);
-  driveTo(300, 300);
-}
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-// "when started" hat block
-int whenStarted1() {
-  pnematicPort.set(false);
-  return 0;
+  intake.setVelocity(100, percent);
 }
 
 // "when ButtonL1 pressed
@@ -156,11 +62,70 @@ void pnematics_pull() {
   pnematicPort.set(false);
 }
 
+void driveTo(int posX, int posY, bool reverse) {
+  // Get current values
+  int currentPosX = 0; // Current Pos to be determined
+  int currentPosY = 0;
+  int currentHeading = 0; // Current heading to be determined
+  float deltaX = posX - currentPosX;
+  float deltaY = posY - currentPosY;
+  
+  // Calculate distance and angle
+  float distance = sqrt(pow(deltaX, 2) + pow(deltaY, 2));
+  float angle = atan2(deltaY, deltaX) * 180 / M_PI;
+  if(reverse == true) {
+    currentHeading -= 180;
+  }
+  if(currentHeading - angle > 180) {
+    currentHeading -= 360;
+  } else if(currentHeading - angle < -180) {
+    currentHeading += 360;
+  }
+  
+  Drivetrain.turnFor(right, angle - currentHeading, degrees);
+  Drivetrain.driveFor(forward, distance, mm);
+  currentPosX = posX;
+  currentPosY = posY;
+  currentHeading = angle;
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................d
+  intake.setVelocity(100, percent);
+  intake.spin(forward);
+  wait(2, sec);
+  intake.stop();
+  wait(2, sec);
+  intake.spin(forward);
+  wait(2, sec);
+  intake.stop();
+  wait(2, sec);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
 void usercontrol(void) {
-  // User control code here, inside the loop
-  whenStarted1();
   while (1) {
-    
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
@@ -171,11 +136,15 @@ void usercontrol(void) {
     // ........................................................................
 
     // calculate the drivetrain motor velocities from the controller joystick axies
-    int drivetrainLeftSideSpeed = (Controller1.Axis3.position() * 0.8) + (Controller1.Axis1.position() * 0.6);
-    int drivetrainRightSideSpeed = (Controller1.Axis3.position() * 0.8) - (Controller1.Axis1.position() * 0.6);
+    float drivetrainLeftSideSpeed = ((Controller1.Axis3.position() * 0.8) + (Controller1.Axis1.position() * 0.6)) * (driveTrainSpeed / 100);
+    float drivetrainRightSideSpeed = ((Controller1.Axis3.position() * 0.8) - (Controller1.Axis1.position() * 0.6)) * (driveTrainSpeed / 100);
 
     // If values in deadband range, stop the drive motors and skip the motor control
-    if (Controller1.Axis1.position() < 5 && Controller1.Axis1.position() > -5 && Controller1.Axis2.position() < 5 && Controller1.Axis2.position() > -5 && Controller1.Axis3.position() < 5 && Controller1.Axis3.position() > -5 && Controller1.Axis4.position() < 5 && Controller1.Axis4.position() > -5) {
+    if (fabs(Controller1.Axis1.position()) < 5 &&
+     fabs(Controller1.Axis2.position()) < 5 &&
+     fabs(Controller1.Axis3.position()) < 5 &&
+     fabs(Controller1.Axis4.position()) < 5) {
+
       // Stop the drive motors
       LeftDriveSmart.stop();
       RightDriveSmart.stop();
@@ -187,24 +156,33 @@ void usercontrol(void) {
       RightDriveSmart.spin(forward);
     }
 
-    // Intake Control
-    if (Controller1.ButtonX.pressing()) {
-      leftIntake.spin(forward);
-      rightIntake.spin(forward);
-    } else if (Controller1.ButtonB.pressing()) {
-      leftIntake.spin(reverse);
-      rightIntake.spin(reverse);
-    } else {
-      leftIntake.stop();
-      rightIntake.stop();
+    // Intake/Hook Control
+    if(Controller1.ButtonR1.pressing()) {
+      hook.spin(forward);
+    } 
+    if(Controller1.ButtonR2.pressing()) {
+      intake.spin(forward);
+    }
+    if(Controller1.ButtonB.pressing() && !Controller1.ButtonR1.pressing() && !Controller1.ButtonR2.pressing()) {
+      hook.spin(reverse);
+      intake.spin(reverse);
     }
 
-    // Pnematics Push/Pull
+    // Pnematics Control
     Controller1.ButtonL1.pressed(pnematics_push);
     Controller1.ButtonL2.pressed(pnematics_pull);
 
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+    // Control drivetrain speed
+    if(Controller1.ButtonRight.pressing()) {
+      driveTrainSpeed = 100;
+    } else if (Controller1.ButtonUp.pressing()) {
+      driveTrainSpeed = 75;
+    } else if (Controller1.ButtonLeft.pressing()) {
+      driveTrainSpeed = 50;
+    }
+
+    // Sleep the task for a short amount of time to prevent wasted resources.
+    wait(20, msec);
   }
 }
 
